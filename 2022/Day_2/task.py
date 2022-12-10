4458ed9a489c4aa70f2dbf40630a85f0c527b592
Szpila123 <bartoszszpila98@gmail.com>
2022-12-10 18:16:34 +0100

    Szpila123/Year_2022: Day 2 done

---
+++ 2022/Day_2/task.py
@@ -0,0 +1,40 @@
+match_results = {
+    ('A', 'X'): 3 + 1,
+    ('A', 'Y'): 6 + 2,
+    ('A', 'Z'): 0 + 3,
+    ('B', 'X'): 0 + 1,
+    ('B', 'Y'): 3 + 2,
+    ('B', 'Z'): 6 + 3,
+    ('C', 'X'): 6 + 1,
+    ('C', 'Y'): 0 + 2,
+    ('C', 'Z'): 3 + 3
+}
+
+move_lookup = {
+    ('A', 'X'): 'Z',
+    ('A', 'Y'): 'X',
+    ('A', 'Z'): 'Y',
+    ('B', 'X'): 'X',
+    ('B', 'Y'): 'Y',
+    ('B', 'Z'): 'Z',
+    ('C', 'X'): 'Y',
+    ('C', 'Y'): 'Z',
+    ('C', 'Z'): 'X' 
+
+}
+
+sum_1, sum_2 = 0, 0
+with open('input.txt') as file:
+    for line in file:
+        opponent, player = line.split()
+        sum_1 += match_results[(opponent, player)]
+        sum_2 += match_results[(opponent, move_lookup[(opponent, player)])]
+
+
+
+
+    
+
+print(sum_1)
+print(sum_2)
+
